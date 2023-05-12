38cda92b788dc1ae25ab744843959f5707ec2db5
Anh Thu Vo <avo@alum.mit.edu>
2023-05-12 00:00:16 -0700

    WIP - Container Starting status

--- free5gc-operator/controllers/free5gc_smfcfg.go
+++ free5gc-operator/controllers/free5gc_smfcfg.go
@@ -27,44 +27,96 @@ info:
 configuration:
   ReportCaller: false
   debugLevel: info
-  pfcp:
-    - addr: {{ .PFCP_IP }}
-
-
-uerouting.yaml: |
-    info:
-      version: 1.0.1
-      description: Routing information for UE
-    ueRoutingInfo:
-      UE1: # Group Name
-        members:
-        - imsi-208930000000003 # Subscription Permanent Identifier of the UE
-        topology: # Network topology for this group (Uplink: A->B, Downlink: B->A)
-        # default path derived from this topology
-        # node name should be consistent with smfcfg.yaml
-          - A: gNB1
-            B: BranchingUPF
-          - A: BranchingUPF
-            B: AnchorUPF1
-        specificPath:
-          - dest: 10.100.100.26/32 # the destination IP address on Data Network (DN)
-            # the order of UPF nodes in this path. We use the UPF's name to represent each UPF node.
-            # The UPF's name should be consistent with smfcfg.yaml
-            path: [BranchingUPF, AnchorUPF2]
+  serviceNameList:
+    - nsmf-pdusession
+    - nsmf-event-exposure
+    - nsmf-oam
       
-      UE2: # Group Name
-        members:
-        - imsi-208930000000004 # Subscription Permanent Identifier of the UE
-        topology: # Network topology for this group (Uplink: A->B, Downlink: B->A)
-        # default path derived from this topology
-        # node name should be consistent with smfcfg.yaml
-          - A: gNB1
-            B: BranchingUPF
-          - A: BranchingUPF
-            B: AnchorUPF1
-        specificPath:
-          - dest: 10.100.100.16/32 # the destination IP address on Data Network (DN)
-            # the order of UPF nodes in this path. We use the UPF's name to represent each UPF node.
-            # The UPF's name should be consistent with smfcfg.yaml
-            path: [BranchingUPF, AnchorUPF2]
+  sbi:
+    scheme: http
+    registerIPv4: smf-nsmf # IP used to register to NRF
+    bindingIPv4: 0.0.0.0  # IP used to bind the service
+    port: 80
+    tls:
+      key: config/TLS/smf.key
+      pem: config/TLS/smf.pem
+      
+  nrfUri: http://nrf-nnrf:8000
+
+  pfcp:
+    addr: {{ .PFCP_IP }}
+  smfName: SMF
+  snssaiInfos:
+    - sNssai:
+        sst: 1
+        sd: 010203
+      dnnInfos: # DNN information list
+        - dnn: internet # Data Network Name
+          dns: # the IP address of DNS
+            ipv4: 8.8.8.8
+    - sNssai:
+        sst: 1
+        sd: 112233
+      dnnInfos: # DNN information list
+        - dnn: internet # Data Network Name
+          dns: # the IP address of DNS
+            ipv4: 8.8.8.8
+    - sNssai:
+        sst: 2
+        sd: 112234    
+      dnnInfos:
+        - dnn: internet
+          dns: 
+            ipv4: 8.8.8.8
+  plmnList: # the list of PLMN IDs that this SMF belongs to (optional, remove this key when unnecessary)
+    - mcc: "208" # Mobile Country Code (3 digits string, digit: 0~9)
+      mnc: "93" # Mobile Network Code (2 or 3 digits string, digit: 0~9)
+  userplaneInformation: # list of userplane information
+    upNodes: # information of userplane node (AN or UPF)
+      gNB1: # the name of the node
+        type: AN # the type of the node (AN or UPF)
+      UPF:  # the name of the node
+        type: UPF # the type of the node (AN or UPF)
+        nodeID: 10.100.50.241 # the IP/FQDN of N4 interface on this UPF (PFCP)
+        sNssaiUpfInfos: # S-NSSAI information list for this UPF
+              - sNssai: # S-NSSAI (Single Network Slice Selection Assistance Information)
+                  sst: 1 # Slice/Service Type (uinteger, range: 0~255)
+                  sd: 010203 # Slice Differentiator (3 bytes hex string, range: 000000~FFFFFF)
+                dnnUpfInfoList: # DNN information list for this S-NSSAI
+                  - dnn: internet
+                    pools:
+                      - cidr: 10.1.0.0/17
+              - sNssai: # S-NSSAI (Single Network Slice Selection Assistance Information)
+                  sst: 1 # Slice/Service Type (uinteger, range: 0~255)
+                  sd: 112233 # Slice Differentiator (3 bytes hex string, range: 000000~FFFFFF)
+                dnnUpfInfoList: # DNN information list for this S-NSSAI
+                  - dnn: internet
+                    pools:
+                      - cidr: 10.1.128.0/17 
+        interfaces: # Interface list for this UPF
+              - interfaceType: N3 # the type of the interface (N3 or N9)
+                endpoints: # the IP address of this N3/N9 interface on this UPF
+                  - 10.100.50.233
+                networkInstance: internet # Data Network Name (DNN)
+    links: # the topology graph of userplane, A and B represent the two nodes of each link
+      - A: gNB1
+        B: UPF
+  locality: area1 # Name of the location where a set of AMF, SMF and UPFs are located
+
+logger:
+  Aper:
+    ReportCaller: false
+    debugLevel: info
+  NAS:
+    ReportCaller: false
+    debugLevel: info
+  NGAP:
+    ReportCaller: false
+    debugLevel: info
+  PFCP:
+    ReportCaller: false
+    debugLevel: info
+  SMF:
+    ReportCaller: false
+    debugLevel: info
 `
--- free5gc-operator/controllers/smfdeployment_controller.go
+++ free5gc-operator/controllers/smfdeployment_controller.go
@@ -111,7 +111,7 @@ func getSMFNad(templateName string, spec *workloadv1alpha1.SMFDeploymentSpec) st
 
 	// n6CfgSlice := getIntConfigSlice(spec.Interfaces, "N6")
 	// n3CfgSlice := getIntConfigSlice(spec.Interfaces, "N3")
-	n4CfgSlice := getIntConfigSlice(spec.Interfaces, "N4")
+	n4CfgSlice := getIntConfigSlice(spec.Interfaces, "n4")
 	// n9CfgSlice := getIntConfigSlice(spec.Interfaces, "N9")
 
 	ret = `[`
@@ -303,6 +303,113 @@ func free5gcSMFCreateConfigmap(logger logr.Logger, smfDeploy *workloadv1alpha1.S
 	smfcfgStruct := SMFcfgStruct{}
 	smfcfgStruct.PFCP_IP = n4IP
 
+	/*
+			apiVersion: v1
+		kind: ConfigMap
+		metadata:
+		  name: smf-configmap
+		  labels:
+		    app: free5gc
+		data:
+		  smfcfg.yaml: |
+		    info:
+		      version: 1.0.2
+		      description: SMF initial local configuration
+
+		    configuration:
+		      serviceNameList:
+		        - nsmf-pdusession
+		        - nsmf-event-exposure
+		        - nsmf-oam
+
+		      sbi:
+		        scheme: http
+		        registerIPv4: smf-nsmf # IP used to register to NRF
+		        bindingIPv4: 0.0.0.0  # IP used to bind the service
+		        port: 80
+		        tls:
+		          key: config/TLS/smf.key
+		          pem: config/TLS/smf.pem
+
+		      nrfUri: http://nrf-nnrf:8000
+
+		      pfcp:
+		        addr: 10.100.50.244
+		      smfName: SMF
+		      snssaiInfos:
+		        - sNssai:
+		            sst: 1
+		            sd: 010203
+		          dnnInfos: # DNN information list
+		            - dnn: internet # Data Network Name
+		              dns: # the IP address of DNS
+		                ipv4: 8.8.8.8
+		        - sNssai:
+		            sst: 1
+		            sd: 112233
+		          dnnInfos: # DNN information list
+		            - dnn: internet # Data Network Name
+		              dns: # the IP address of DNS
+		                ipv4: 8.8.8.8
+		        - sNssai:
+		            sst: 2
+		            sd: 112234
+		          dnnInfos:
+		            - dnn: internet
+		              dns:
+		                ipv4: 8.8.8.8
+		      plmnList: # the list of PLMN IDs that this SMF belongs to (optional, remove this key when unnecessary)
+		        - mcc: "208" # Mobile Country Code (3 digits string, digit: 0~9)
+		          mnc: "93" # Mobile Network Code (2 or 3 digits string, digit: 0~9)
+		      userplaneInformation: # list of userplane information
+		        upNodes: # information of userplane node (AN or UPF)
+		          gNB1: # the name of the node
+		            type: AN # the type of the node (AN or UPF)
+		          UPF:  # the name of the node
+		            type: UPF # the type of the node (AN or UPF)
+		            nodeID: 10.100.50.241 # the IP/FQDN of N4 interface on this UPF (PFCP)
+		            sNssaiUpfInfos: # S-NSSAI information list for this UPF
+		                  - sNssai: # S-NSSAI (Single Network Slice Selection Assistance Information)
+		                      sst: 1 # Slice/Service Type (uinteger, range: 0~255)
+		                      sd: 010203 # Slice Differentiator (3 bytes hex string, range: 000000~FFFFFF)
+		                    dnnUpfInfoList: # DNN information list for this S-NSSAI
+		                      - dnn: internet
+		                        pools:
+		                          - cidr: 10.1.0.0/17
+		                  - sNssai: # S-NSSAI (Single Network Slice Selection Assistance Information)
+		                      sst: 1 # Slice/Service Type (uinteger, range: 0~255)
+		                      sd: 112233 # Slice Differentiator (3 bytes hex string, range: 000000~FFFFFF)
+		                    dnnUpfInfoList: # DNN information list for this S-NSSAI
+		                      - dnn: internet
+		                        pools:
+		                          - cidr: 10.1.128.0/17
+		            interfaces: # Interface list for this UPF
+		                  - interfaceType: N3 # the type of the interface (N3 or N9)
+		                    endpoints: # the IP address of this N3/N9 interface on this UPF
+		                      - 10.100.50.233
+		                    networkInstance: internet # Data Network Name (DNN)
+		        links: # the topology graph of userplane, A and B represent the two nodes of each link
+		          - A: gNB1
+		            B: UPF
+		      locality: area1 # Name of the location where a set of AMF, SMF and UPFs are located
+
+		    logger:
+		      Aper:
+		        ReportCaller: false
+		        debugLevel: info
+		      NAS:
+		        ReportCaller: false
+		        debugLevel: info
+		      NGAP:
+		        ReportCaller: false
+		        debugLevel: info
+		      PFCP:
+		        ReportCaller: false
+		        debugLevel: info
+		      SMF:
+		        ReportCaller: false
+		        debugLevel: info
+	*/
 	smfcfgTemplate := template.New("SMFCfg")
 	smfcfgTemplate, err = smfcfgTemplate.Parse(SMFCfgTemplate)
 	if err != nil {
@@ -317,15 +424,15 @@ func free5gcSMFCreateConfigmap(logger logr.Logger, smfDeploy *workloadv1alpha1.S
 		return nil, err
 	}
 
-	var smf_uerouting bytes.Buffer
-	if err := smfueroutingTemplate.Execute(&smf_uerouting, smfcfgStruct); err != nil {
-		log.Log.Info("Could not render smf_uerouting template.")
-		return nil, err
-	}
-
 	var smfcfg bytes.Buffer
 	if err := smfcfgTemplate.Execute(&smfcfg, smfcfgStruct); err != nil {
 		log.Log.Info("Could not render SMFCfgTemplate template.")
+		return nil, err
+	}
+
+	var smf_uerouting bytes.Buffer
+	if err := smfueroutingTemplate.Execute(&smf_uerouting, smfcfgStruct); err != nil {
+		log.Log.Info("Could not render smf_uerouting template.")
 		return nil, err
 	}
 
